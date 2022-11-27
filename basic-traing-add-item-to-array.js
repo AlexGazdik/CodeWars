71775c87fd419431e35b6ec897bb36d1e611c7c0
alex <45057376+AlexGazdik@users.noreply.github.com>
2022-11-27 02:22:26 -0500

    Update basic-traing-add-item-to-array.js

--- basic-traing-add-item-to-array.js
+++ basic-traing-add-item-to-array.js
@@ -1,7 +1,7 @@
-Add the value "codewars" to the websites array.
-After your code executes the websites array should == ["codewars"]
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
 
-The websites array has already been defined for you using the following code:
+// The websites array has already been defined for you using the following code:
 
 var websites = [];
 
