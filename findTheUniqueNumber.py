7bfff58197a6f8ff58db5dada6fd243b9b991420
alex <45057376+AlexGazdik@users.noreply.github.com>
2022-12-03 03:04:17 -0500

    Update findTheUniqueNumber.py

--- findTheUniqueNumber.py
+++ findTheUniqueNumber.py
@@ -1,17 +1,17 @@
-'''There is an array with some numbers. All numbers are equal except for one. Try to find it!
+# '''There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
-find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
-find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+# find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+# find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
 
-It’s guaranteed that array contains at least 3 numbers.
+# It’s guaranteed that array contains at least 3 numbers.
 
-The tests contain some very huge arrays, so think about performance.
+# The tests contain some very huge arrays, so think about performance.
 
-This is the first kata in series:
+# This is the first kata in series:
 
-    Find the unique number (this kata)
-    Find the unique string
-    Find The Unique'''
+#     Find the unique number (this kata)
+#     Find the unique string
+#     Find The Unique'''
 
 
 
